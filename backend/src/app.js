b2358948eb4e96dca365080b2244d3302212f472
Diogo Silva <dihogohsilva@gmail.com>
2026-02-07 22:53:29 -0300

    refactor: clean auth routes and improve responsive layout

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,11 +8,24 @@ const publicPath = path.join(__dirname, "..", "..", "frontend");
 
 app.use(express.json());
 app.use(express.static(publicPath));
-app.use(cors());
-app.use("/api",router);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(publicPath, "login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(publicPath, "register.html"));
+});
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.use(cors());
+app.use("/api", router);
 
 module.exports = app;
